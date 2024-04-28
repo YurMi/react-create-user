f7bf3dce0cee4099b5d1f08ba4f30d0d9b0b3b8c
YurMi <70280514+YurMi@users.noreply.github.com>
2024-04-28 17:02:31 +0300

    Update Popup.jsx

--- src/components/UI/Popup.jsx
+++ src/components/UI/Popup.jsx
@@ -1,1 +1,19 @@
-export default function Popup(props){	const {popupState, setPopupState} = props	return (		<div className={`popup ${popupState ? 'open' : ''}`} onClick={() => setPopupState(false)}>			<div className="popup__body">				<div className="popup__content" onClick={(e) => e.stopPropagation()}>					<button						type="button"						className="popup__close close-popup"						onClick={() => setPopupState(false)}					>X					</button>					{props.children}				</div>			</div>		</div>	)}
+export default function Popup(props){
+	const {popupState, setPopupState} = props
+
+	return (
+		<div className={`popup ${popupState ? 'open' : ''}`} onClick={() => setPopupState(false)}>
+			<div className="popup__body">
+				<div className="popup__content" onClick={(e) => e.stopPropagation()}>
+					<button
+						type="button"
+						className="popup__close close-popup"
+						onClick={() => setPopupState(false)}
+					>X
+					</button>
+					{props.children}
+				</div>
+			</div>
+		</div>
+	)
+}
